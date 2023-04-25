c0eaf9cbeb072cea2d10f10eb31e254e7663f2d7
Akshayraykar <125906371+Akshayraykar@users.noreply.github.com>
2023-04-25 11:42:17 +0530

    Delete program5.java

--- program5.java
+++
@@ -1,9 +0,0 @@
-// error both creation & intilization are not allowed in array
-class RCB{
-	public static void main(string[]args){
-
-		int arr[] = new int[4]{10,20,30,40};
-
-		System.out.println(arr[2]);
-	}
-}
